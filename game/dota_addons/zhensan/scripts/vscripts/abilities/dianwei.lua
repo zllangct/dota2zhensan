77e2f4957ea0f67545602586b6d44fc008943007
Xavier Tseng <364671459@qq.com>
2015-03-18 10:19:55 +0800

    技能重写 01

--- game/dota_addons/zhensan/scripts/vscripts/abilities/dianwei.lua
+++ game/dota_addons/zhensan/scripts/vscripts/abilities/dianwei.lua
@@ -1,6 +1,7 @@
 --[[
     典韦 野性战魂
     设置基础生命恢复速度提升（具体的数值需要再调整一下，我按原来的公式了）
+    -- TODO，提升基础生命恢复速度百分比，应该用 MODIFIER_PROPERTY_HEALTH_REGEN_PERCENTAGE 即可？
     Author: XavierCHN@2015.3.18
 ]]
 function OnDianweiUpdateYexingzhanhun(keys)
@@ -15,47 +16,25 @@ function OnDianweiUpdateYexingzhanhun(keys)
     caster:SetBaseHealthRegen(health_regen_new)
 end
 
---[[
-function dianwei_yexingzhanhun_01(keys)
-	-- body
-	local caster=EntIndexToHScript(keys.caster_entindex)
-    local i=0
-    GameRules:GetGameModeEntity():SetContextThink(DoUniqueString("dianwei"),
-    	function ( )
-    		-- body
-    	
-           if i ~= keys.ability:GetLevel() then 
-           	   i = keys.ability:GetLevel()
-               local health_bonus=keys.ability:GetLevelSpecialValueFor("health_bonus",i-1)
-            -- local health_regen=caster:GetHealthRegen()
-               local health_regen_base=caster:GetBaseHealthRegen()    
-               local base_health_new =health_regen_base + 0.25*(health_bonus/100)
-               caster:SetBaseHealthRegen(base_health_new)
-            end
-           return 1
-
-        end)
-end
-]]
 function AmplifyDamageParticle( event )
-  local target = event.target
-  local location = target:GetAbsOrigin()
-  local particleName = "particles/dianweiyaohuo.vpcf"
+    local target = event.target
+    local location = target:GetAbsOrigin()
+    local particleName = "particles/dianweiyaohuo.vpcf"
 
--- Particle. Need to wait one frame for the older particle to be destroyed
-  Timers:CreateTimer(0.01, function() 
-    target.AmpDamageParticle = ParticleManager:CreateParticle(particleName, PATTACH_OVERHEAD_FOLLOW, target)
-    ParticleManager:SetParticleControl(target.AmpDamageParticle, 0, target:GetAbsOrigin())
-   --ParticleManager:SetParticleControl(target.AmpDamageParticle, 1, target:GetAbsOrigin())
-    --ParticleManager:SetParticleControl(target.AmpDamageParticle, 2, target:GetAbsOrigin())
+    -- Particle. Need to wait one frame for the older particle to be destroyed
+    Timers:CreateTimer(0.01, function() 
+        target.AmpDamageParticle = ParticleManager:CreateParticle(particleName, PATTACH_OVERHEAD_FOLLOW, target)
+        ParticleManager:SetParticleControl(target.AmpDamageParticle, 0, target:GetAbsOrigin())
+        --ParticleManager:SetParticleControl(target.AmpDamageParticle, 1, target:GetAbsOrigin())
+        --ParticleManager:SetParticleControl(target.AmpDamageParticle, 2, target:GetAbsOrigin())
 
-    ParticleManager:SetParticleControlEnt(target.AmpDamageParticle, 1, target, PATTACH_OVERHEAD_FOLLOW, "attach_overhead", target:GetAbsOrigin(), true)
-   --ParticleManager:SetParticleControlEnt(target.AmpDamageParticle, 2, target, PATTACH_OVERHEAD_FOLLOW, "attach_overhead""attach_hitloc", target:GetAbsOrigin(), true)
-  end)
+        ParticleManager:SetParticleControlEnt(target.AmpDamageParticle, 1, target, PATTACH_OVERHEAD_FOLLOW, "attach_overhead", target:GetAbsOrigin(), true)
+        --ParticleManager:SetParticleControlEnt(target.AmpDamageParticle, 2, target, PATTACH_OVERHEAD_FOLLOW, "attach_overhead""attach_hitloc", target:GetAbsOrigin(), true)
+    end)
 end
 
 -- Destroys the particle when the modifier is destroyed
 function EndAmplifyDamageParticle( event )
-  local target = event.target
-  ParticleManager:DestroyParticle(target.AmpDamageParticle,false)
+    local target = event.target
+    ParticleManager:DestroyParticle(target.AmpDamageParticle,false)
 end
--- game/dota_addons/zhensan/scripts/vscripts/abilities/guanyu.lua
+++ game/dota_addons/zhensan/scripts/vscripts/abilities/guanyu.lua
@@ -1,73 +1,73 @@
 require("utils/utils_print")
 function guanyu_liuyuefeizhan_01( keys)
-	local caster=EntIndexToHScript(keys.caster_entindex)
-	local ability=keys.ability
-	local caster_ori = caster:GetAbsOrigin()
-    local point =keys.target_points[1]
-    local distance = (caster_ori-point):Length()
-    local movement = distance/1
-    local dir=(caster_ori-point):Normalized()
-    local k=keys.ability:GetLevel()
-    local damage=keys.ability:GetLevelSpecialValueFor("damage",k-1)
-    guanyu_liuyuefeizhan_move(caster,point,movement,distance,dir,damage,ability)
+    local caster=EntIndexToHScript(keys.caster_entindex)
+    local ability=keys.ability
+    local caster_ori = caster:GetAbsOrigin()
+      local point =keys.target_points[1]
+      local distance = (caster_ori-point):Length()
+      local movement = distance/1
+      local dir=(caster_ori-point):Normalized()
+      local k=keys.ability:GetLevel()
+      local damage=keys.ability:GetLevelSpecialValueFor("damage",k-1)
+      guanyu_liuyuefeizhan_move(caster,point,movement,distance,dir,damage,ability)
 end 
-function guanyu_liuyuefeizhan_move(caster,point,movement,distance,dir,damage,ability)  --耗时0.5秒
-	-- body
-	  local caster_ori = caster:GetAbsOrigin()
-	  local perdistance_h = distance / 40
-	  local cont = 0
-      local perdistance_v  = 10
-      GameRules:GetGameModeEntity():SetContextThink(DoUniqueString("fengbaozhizhang_02"),function()  	 
-	    local caster_ori = caster:GetAbsOrigin()
-	    caster:SetAbsOrigin(caster_ori-dir*perdistance_h)
-	    caster_ori = caster:GetAbsOrigin() 
-        if cont < 20 then 
-        	caster:SetAbsOrigin(caster_ori+Vector(0,0,perdistance_v))
-        else 
-        	caster:SetAbsOrigin(caster_ori-Vector(0,0,2*perdistance_v))
-        end
-        if cont == 29 then 
-            caster:SetAbsOrigin(GetGroundPosition(caster_ori-Vector(0,0,perdistance_v),caster))
-        end   
-	    cont=cont+1
-	    if cont == 30 then 
-	    	local p_end = 'particles/econ/items/brewmaster/brewmaster_offhand_elixir/brewmaster_thunder_clap_elixir.vpcf'
-            local p_index = ParticleManager:CreateParticle(p_end, PATTACH_CUSTOMORIGIN, caster)
-            ParticleManager:SetParticleControl(p_index, 0, caster:GetOrigin())
-            
-         guanyu_liuyuefeizhan_damage(caster,damage,point,ability)
-         FindClearSpaceForUnit(caster, caster:GetAbsOrigin(), false)
-         ParticleManager:ReleaseParticleIndex(p_index)
-         return nil
-	    end 
-	    return 0.02
-    end,0)
+function guanyu_liuyuefeizhan_move(caster,point,movement,distance,dir,damage,ability)   --耗时0.5秒
+    -- body
+    local caster_ori = caster:GetAbsOrigin()
+    local perdistance_h = distance / 40
+    local cont = 0
+    local perdistance_v   = 10
+    GameRules:GetGameModeEntity():SetContextThink(DoUniqueString("fengbaozhizhang_02"),function()    
+          local caster_ori = caster:GetAbsOrigin()
+          caster:SetAbsOrigin(caster_ori-dir*perdistance_h)
+          caster_ori = caster:GetAbsOrigin() 
+            if cont < 20 then 
+                caster:SetAbsOrigin(caster_ori+Vector(0,0,perdistance_v))
+            else 
+                caster:SetAbsOrigin(caster_ori-Vector(0,0,2*perdistance_v))
+            end
+            if cont == 29 then 
+                  caster:SetAbsOrigin(GetGroundPosition(caster_ori-Vector(0,0,perdistance_v),caster))
+            end  
+          cont=cont+1
+          if cont == 30 then 
+           local p_end = 'particles/econ/items/brewmaster/brewmaster_offhand_elixir/brewmaster_thunder_clap_elixir.vpcf'
+                  local p_index = ParticleManager:CreateParticle(p_end, PATTACH_CUSTOMORIGIN, caster)
+                  ParticleManager:SetParticleControl(p_index, 0, caster:GetOrigin())
+                  
+             guanyu_liuyuefeizhan_damage(caster,damage,point,ability)
+             FindClearSpaceForUnit(caster, caster:GetAbsOrigin(), false)
+             ParticleManager:ReleaseParticleIndex(p_index)
+             return nil
+          end 
+          return 0.02
+      end,0)
 end
 function guanyu_liuyuefeizhan_damage(caster,damage,point,ability)
-	-- body
-	print("enter damage")
-	local target=FindUnitsInRadius(caster:GetTeam(), point, nil, 300, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), DOTA_UNIT_TARGET_FLAG_NONE, 0, false)
-	PrintTable(target)
-	local caster_now=caster:GetAbsOrigin() 
-	print("castar_now:"..tostring(castar_now))   
-	if target[1] then 
-	  for _,_target in pairs(target) do
-		local target_now =_target:GetAbsOrigin()
-		print("_target_now:"..tostring(_target_now))
-		local _distance = (caster_now-target_now):Length() 
-		local _damage = (damage-100)*(_distance/300)+100   
-		    ApplyDamage(
-                                { victim = _target, 
-                                  attacker = caster, 
-                                  damage = _damage,	
-                                  damage_type = DAMAGE_TYPE_PHYSICAL 
-                                }
-			            )
+    -- body
+    print("enter damage")
+    local target=FindUnitsInRadius(caster:GetTeam(), point, nil, 300, ability:GetAbilityTargetTeam(), ability:GetAbilityTargetType(), DOTA_UNIT_TARGET_FLAG_NONE, 0, false)
+    PrintTable(target)
+    local caster_now=caster:GetAbsOrigin() 
+    print("castar_now:"..tostring(castar_now))     
+    if target[1] then 
+      for _,_target in pairs(target) do
+     local target_now =_target:GetAbsOrigin()
+     print("_target_now:"..tostring(_target_now))
+     local _distance = (caster_now-target_now):Length() 
+     local _damage = (damage-100)*(_distance/300)+100  
+           ApplyDamage(
+                                                { victim = _target, 
+                                                    attacker = caster, 
+                                                    damage = _damage, 
+                                                    damage_type = DAMAGE_TYPE_PHYSICAL 
+                                                }
+                         )
 
-		    
-	  end
-   end
-    if caster:HasModifier("guanyu_liuyuefeizhan_wudi")  then 
-    	caster:RemoveModifierByName("guanyu_liuyuefeizhan_wudi") 
-    end
+           
+      end
+     end
+      if caster:HasModifier("guanyu_liuyuefeizhan_wudi")  then 
+          caster:RemoveModifierByName("guanyu_liuyuefeizhan_wudi") 
+      end
 end
