1b867a7f7fbccb4d18b1e2bcd21a6db454ef43c6
Xavier Tseng <364671459@qq.com>
2015-03-21 14:21:02 +0800

    更新两个弹出文字的API

--- game/dota_addons/zhensan/scripts/vscripts/hud/HudGameEvents.lua
+++ game/dota_addons/zhensan/scripts/vscripts/hud/HudGameEvents.lua
@@ -15,3 +15,34 @@ function UpdateMoraleData(moraleTeam1, moraleTeam2)
         MoraleWei = moraleTeam2,
     })
 end
+
+-- 为某个玩家在某个单位的位置弹出一个图形提示 @ todo 暂时无用
+function PopupImageForPlayer(target, imageName, playerID)
+    local pos = target:GetOrigin()
+    local x = pos.x
+    local y = pos.y
+    local z = pos.z
+    
+    FireGameEvent("show_popup_image",{
+        _x = x,
+        _y = y,
+        _z = z,
+        _playerID = playerID,
+        _image_name = imageName
+    })
+end
+
+-- 为所有玩家在某个单位的位置弹出一个图标 @ todo 暂时无用
+function PopupImageAll(target, imageName)
+    local pos = target:GetOrigin()
+    local x = pos.x
+    local y = pos.y
+    local z = pos.z
+    
+    FireGameEvent("show_popup_image_all",{
+        _x = x,
+        _y = y,
+        _z = z,
+        _image_name = imageName
+    })
+end
